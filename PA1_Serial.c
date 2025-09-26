1e0d237f65361c369abdc15f9c39fa67b1e739b7
pathey <peter@athey.org>
2025-09-26 13:35:10 -0700

    Added .gitignore. Improved features of serial run. Set up a few components for building the parallel implementations.

--- PA1_Serial.c
+++ PA1_Serial.c
@@ -6,9 +6,22 @@ int main (int argc, char *argv[]){
 	int N;
 	int sum = 0;
 	int i;
+	time_t start, end;
+
 	sscanf(argv[1], "%d", &N);
+	
+	start = clock();
+
 	for (i=0; i <= N; i++){
 		sum += i;
 	}
-	printf("Sum: %d \n", sum);
+
+	end = clock();
+
+	double elapsed = (double)(end - start) / (double)(CLOCKS_PER_SEC);
+	
+	printf("%.6f seconds elapsed\n", elapsed);	
+	printf("Sum: %d \n \n", sum);
+
+	
 }
