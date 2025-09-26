45310ad67c844e337913ca75f201e0c72aa9a280
aperture <armonaDC@gmail.com>
2025-09-26 01:19:33 -0700

    fix to large sum error

--- PA1_Serial.c
+++ PA1_Serial.c
@@ -3,13 +3,12 @@
 #include <time.h>
 
 int main (int argc, const char *argv[]){
-	int i;
-    unsigned long long int sum = 0, N;
+    unsigned long long int sum = 0, N, i;
 
-	sscanf(argv[1], "%lld", &N);
+    N = strtoull(argv[1], NULL, 10); //string to ull (unsigned long long)
 
 	for (i=0; i < N; i++){ //non-inclusive sum
 		sum += i;
 	}
-	printf("Sum from 1 to %lld (non-inclusive): %lld \n", N, sum);
+	printf("Sum from 1 to %llu (non-inclusive): %llu \n", N, sum);
 }
